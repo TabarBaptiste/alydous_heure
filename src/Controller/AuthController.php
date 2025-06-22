bb0eec80fd034805f887c642f0f4636b910ddb83
Baptiste <tabarbaptiste@gmail.com>
2025-06-22 22:57:42 +0200

    Ajout du contrôleur AuthController avec une méthode d'inscription pour les utilisateurs, incluant la gestion du mot de passe et la réponse JSON.

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class AuthController extends AbstractController
+{
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setNom($data['nom']);
+        $user->setPrenom($data['prenom']);
+        $user->setTelephone($data['telephone'] ?? null);
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $hasher->hashPassword($user, $data['password'])
+        );
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Inscription réussie'], 201);
+    }
+}
