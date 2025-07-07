e4907c571e4b1b9bebaa80f09d4d29935b922041
Baptiste <tabarbaptiste@gmail.com>
2025-07-07 17:46:00 -0400

    Ajout de la vérification de l'existence d'un utilisateur par e-mail lors de l'inscription, avec gestion des erreurs appropriée.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +15,18 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 final class AuthController extends AbstractController
 {
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
-    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): JsonResponse
-    {
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $hasher,
+        UserRepository $userRepo
+    ): JsonResponse {
         $data = json_decode($request->getContent(), true);
+
+        // Vérifier si un utilisateur avec cet e-mail existe
+        if ($userRepo->findOneBy(['email' => $data['email']])) {
+            return new JsonResponse(['error' => 'Un compte avec cet e-mail existe déjà.'], 409);
+        }
 
         $user = new User();
         $user->setEmail($data['email']);
